d6196f9e2a04293885c146624dd4814e17510f11
dkmos2016 <dkmos2016@126.com>
2022-09-21 21:59:36 +0800

    更新linux构建脚本

--- build.sh
+++ build.sh
@@ -22,7 +22,13 @@ mkdir -p ./usr/optional/libgcc_s/
 mkdir -p ./usr/plugins/platforminputcontexts/
 wget -c https://github.com/dkmos2016/ext-linux-bin/raw/main/appimage/AppRun-patched-x86_64 -O AppRun
 wget -c https://github.com/dkmos2016/ext-linux-bin/raw/main/appimage/exec-x86_64.so -O ./usr/optional/exec.so
+
+# need Qt5_15
 wget https://github.com/Qv2ray/qt-pkg/releases/download/bruh/libfcitx5platforminputcontextplugin.so -O ./usr/plugins/platforminputcontexts/libfcitx5platforminputcontextplugin.so
+
+
+# 根据libfcitx5platforminputcontextplugin.so选择Qt版本
+cp -rv /usr/lib/x86_64-linux-gnu/qt5/plugins/platforminputcontexts/libfcitx5platforminputcontextplugin.so ./usr/plugins/platforminputcontexts/libfcitx5platforminputcontextplugin.so 
 
 cp -fv /usr/lib/x86_64-linux-gnu/libstdc++.so.6 ./usr/optional/libstdc++/
 cp -fv /lib/x86_64-linux-gnu/libgcc_s.so.1 ./usr/optional/libgcc_s/
