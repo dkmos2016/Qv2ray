9e5aa28fffd7ff01b964433fd90f2fb277a21bb8
dkmos2016 <dkmos2016@126.com>
2022-09-21 18:18:30 +0800

    修正编译脚本

--- build.sh
+++ build.sh
@@ -5,6 +5,9 @@ cd build
 
 echo "WORKSPACE: `pwd`"
 
+QMAKE_PATH=/opt/tools/Qt5.12.8/5.12.8/gcc_64/bin/qmake
+echo "qmake: ${QMAKE_PATH}"
+
 cmake .. -GNinja \
     -DCMAKE_INSTALL_PREFIX=./AppDir/usr \
     -DCMAKE_BUILD_TYPE=Release \
@@ -17,8 +20,8 @@ mkdir -p ./usr/{lib,optional}/
 mkdir -p ./usr/optional/libstdc++/
 mkdir -p ./usr/optional/libgcc_s/
 mkdir -p ./usr/plugins/platforminputcontexts/
-wget -c https://github.com/dkmos2016/ext-linux-bin/blob/main/appimage/AppRun-patched-x86_64 -O AppRun
-wget -c https://github.com/dkmos2016/ext-linux-bin/blob/main/appimage/exec-x86_64.so -O ./usr/optional/exec.so
+wget -c https://github.com/dkmos2016/ext-linux-bin/raw/main/appimage/AppRun-patched-x86_64 -O AppRun
+wget -c https://github.com/dkmos2016/ext-linux-bin/raw/main/appimage/exec-x86_64.so -O ./usr/optional/exec.so
 wget https://github.com/Qv2ray/qt-pkg/releases/download/bruh/libfcitx5platforminputcontextplugin.so -O ./usr/plugins/platforminputcontexts/libfcitx5platforminputcontextplugin.so
 
 cp -fv /usr/lib/x86_64-linux-gnu/libstdc++.so.6 ./usr/optional/libstdc++/
@@ -27,12 +30,21 @@ cp -fv /usr/lib/x86_64-linux-gnu/{libssl.so.1.1,libcrypto.so.1.1} ./usr/lib/
 cp -fv /usr/lib/x86_64-linux-gnu/qt5/plugins/platforminputcontexts/libfcitxplatforminputcontextplugin.so ./usr/plugins/platforminputcontexts/
 chmod a+x AppRun
 cd ..
-wget https://github.com/probonopd/linuxdeployqt/releases/download/6/linuxdeployqt-6-x86_64.AppImage
-chmod +x ./linuxdeployqt-6-x86_64.AppImage
-./linuxdeployqt-6-x86_64.AppImage AppDir/usr/share/applications/qv2ray.desktop \
+# wget https://github.com/probonopd/linuxdeployqt/releases/download/6/linuxdeployqt-6-x86_64.AppImage
+# chmod +x ./linuxdeployqt-6-x86_64.AppImage
+# ./linuxdeployqt-6-x86_64.AppImage AppDir/usr/share/applications/qv2ray.desktop \
+#     -appimage \
+#     -no-strip \
+#     -always-overwrite \
+#     -extra-plugins=iconengines,imageformats,platforminputcontexts
+
+wget https://github.com/probonopd/linuxdeployqt/releases/download/5/linuxdeployqt-5-x86_64.AppImage
+chmod +x ./linuxdeployqt-5-x86_64.AppImage
+./linuxdeployqt-5-x86_64.AppImage AppDir/usr/share/applications/qv2ray.desktop \
     -appimage \
     -no-strip \
     -always-overwrite \
-    -extra-plugins=iconengines,imageformats,platforminputcontexts
+    -extra-plugins=iconengines,imageformats,platforminputcontexts \
+    -qmake=$QMAKE_PATH
 
 mv ./Qv2ray*.AppImage ./Qv2ray.AppImage
